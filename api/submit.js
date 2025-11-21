ded511efe5f67f9afd8c4a55cf89277a4c55fa4f
wdomi <20965917+wdomi@users.noreply.github.com>
2025-11-21 17:16:06 +0100

    changed from working

--- api/submit.js
+++ api/submit.js
@@ -1,15 +1,18 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Method not allowed" });
 
   const BASEROW_TOKEN = process.env.BASEROW_TOKEN;
-  const BASEROW_ENDPOINT = "https://api.baserow.io/api/database/rows/table/742957/?user_field_names=true";
+  const BASEROW_ENDPOINT =
+    "https://api.baserow.io/api/database/rows/table/742957/?user_field_names=true";
 
   try {
     const payload = req.body;
+
     const response = await fetch(BASEROW_ENDPOINT, {
       method: "POST",
       headers: {
-        "Authorization": "Token " + BASEROW_TOKEN,
+        Authorization: "Token " + BASEROW_TOKEN,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(payload),
